6418b92dfdecbc11ab9fdaa25a7c6decfe331f4c
Toronto <Toronto@28154.com>
2022-09-02 19:08:18 +0800

    first commit

---
+++ 黑马-小程序-商城/代码/main.js
@@ -0,0 +1,61 @@
+import App from './App'
+
+// 1. 导入 store 的实例对象
+import store from './store/store.js'
+
+// #ifndef VUE3
+import Vue from 'vue'
+Vue.config.productionTip = false
+App.mpType = 'app'
+const app = new Vue({
+  ...App,
+  // 2. 将 store 挂载到 Vue 实例上
+  store,
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import {
+  createSSRApp
+} from 'vue'
+export function createApp() {
+  const app = createSSRApp(App)
+  return {
+    app
+  }
+}
+// #endif
+
+
+// 按需导入 $http 对象
+import {
+  $http
+} from '@escook/request-miniprogram'
+
+// 在 uni-app 项目中，可以把 $http 挂载到 uni 顶级对象之上，方便全局调用(因为这里的顶级对象是uni，如果在微信小程序里的话是wx)
+uni.$http = $http
+
+// 配置请求根路径
+$http.baseUrl = 'https://api-ugo-web.itheima.net'
+
+// 请求开始之前做一些事情
+$http.beforeRequest = function(options) {
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+}
+
+// 请求完成之后做一些事情
+$http.afterRequest = function() {
+  uni.hideLoading()
+}
+
+// 封装的展示消息提示的方法
+uni.$showMsg = function(title = '数据加载失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none',
+  })
+}
---
+++ 黑马-小程序-商城/代码/pages/my/my.vue
@@ -0,0 +1,39 @@
+<template>
+  <view>
+    <!-- 用户未登录时，显示登录组件 -->
+    <my-login v-if="!token"></my-login>
+    <!-- 用户登录后，显示用户信息组件 -->
+    <my-userinfo v-else></my-userinfo>
+    <!-- <my-userinfo ></my-userinfo> -->
+    <!-- 176  如果是登录失败的话，在使用updataToken，传递参数时随便传一个字符串就可以了.这样就可以进入我的界面了 -->
+  </view>
+</template>
+
+<script>
+  // 导入自己封装的 mixin 模块
+  import badgeMix from '@/mixins/tabbar-badge.js'
+  // 1. 从 vuex 中按需导入 mapState 辅助函数
+  import {
+    mapState
+  } from 'vuex'
+  export default {
+    // 将 badgeMix 混入到当前的页面中进行使用
+    mixins: [badgeMix],
+    data() {
+      return {
+
+      };
+    },
+    computed: {
+      // 2. 从 m_user 模块中导入需要的 token 字符串
+      ...mapState('m_user', ['token']),
+    },
+  }
+</script>
+
+<style lang="scss">
+page,
+.my-container {
+  height: 100%;
+}
+</style>
